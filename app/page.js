485fff7faaee857de5847d766338c2b056835ccb
SmaranReddy <cs23b1011@iiitr.ac.in>
2024-10-03 11:34:49 +0530

    firstpro

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './_components/Header'
+import Hero from './_components/Hero'
+import Footer from './_components/Footer'
+
+function page() {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default page
